8dacfc2cc7fb7d3b8db13282ecf3ad777f98d182
CandyRagi <tiwariansh935@gmail.com>
2025-10-20 18:59:26 -0700

    intialized profile screen

---
+++ app/src/main/java/com/group_7/studysage/ui/screens/viewmodels/AuthViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.group_7.studysage.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.group_7.studysage.data.repository.AuthRepository
+
+class AuthViewModelFactory(
+    private val authRepository: AuthRepository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(AuthViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return AuthViewModel(authRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
