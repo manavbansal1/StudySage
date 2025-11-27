66dba2d502058375c207fde244e2eb7a7c05828c
Candy <tiwariansh935@gmail.com>
2025-11-26 19:40:07 -0800

    fixed nfc sharing note (creating a copy with same tags and copntent but diffrent noteid)

--- app/src/main/java/com/group_7/studysage/data/nfc/NFCPayload.kt
+++ app/src/main/java/com/group_7/studysage/data/nfc/NFCPayload.kt
@@ -5,10 +5,13 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class NFCPayload(
+    val noteId: String,
     val noteTitle: String,
     val fileUrl: String,
     val originalFileName: String,
     val fileType: String,
+    val content: String = "",
+    val tags: List<String> = emptyList(),
     val courseId: String = "", // Will be set by receiver
     val timestamp: Long = System.currentTimeMillis()
 )
